819fe82dfb18f1ef7611e344ab58fff6505d32f8
ketki kumeriya <111123802+ketkikumeriya@users.noreply.github.com>
2025-06-15 18:15:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+    const board = document.getElementById('board');
+    const BOX_COUNT = 800;
+
+    for (let i = 0; i < BOX_COUNT; i++) {
+      const square = document.createElement('div');
+      square.classList.add('square');
+
+      square.addEventListener('mouseover', () => {
+        square.classList.add('hovered');
+        setTimeout(() => {
+          square.classList.remove('hovered');
+        }, 1000);
+      });
+
+      board.appendChild(square);
+	}
